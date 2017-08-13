0275bf200f49d1e9ebb018a669eafadc6bb5af62
Abhishek Ranjan <abhishek.ism281@gmail.com>
2017-08-13 17:08:52 +0530

    Predicting accuracy assuming no one survived

--- Titanic_survival_exploration.py
+++ Titanic_survival_exploration.py
@@ -26,10 +26,25 @@ def accuracy_score(truth, pred):
     #Returns accuracy score for input truth and predictions.
     # Ensure that the number of predictions matches number of outcomes
     if len(truth) == len(pred):
+        
         # Calculate and return the accuracy as a percent
         return "Predictions have an accuracy of {:.2f}%".format((truth == pred).mean()*100)
     else:
+        #print len(truth), ",", len(pred)
         return "Number of predictions does not match number of outcomes!"
 # Test the 'accuracy_score' function
 predictions = pd.Series(np.ones(5, dtype = int))
 print accuracy_score(outcomes[:5], predictions)
+
+def predictions_0(data):
+    #Model with no features. Always predicts a passenger did not survive.
+    predictions = []
+    for _, passenger in data.iterrows():
+        # Predict the survival of 'passenger'
+        predictions.append(0)
+    # Return our predictions
+    return pd.Series(predictions)
+# Make the predictions
+predictions = predictions_0(data)
+#Score for the above model
+print accuracy_score(outcomes, predictions)
